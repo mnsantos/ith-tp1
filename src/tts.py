66474294b06d15aaeedcc33b8f8f4b703d6574b9
mnsantos <martin.n.santos@gmail.com>
2014-10-06 03:35:22 -0300

    add_prosodia version 1

--- src/tts.py
+++ src/tts.py
@@ -59,8 +59,7 @@ def gen_script(array, output):
 
 class PitchTier:
   def __init__(self):
-    self.points = []
-    self.values = []
+    self.values = {}
     self.xmin = ""
     self.xmax = ""
 
@@ -69,18 +68,20 @@ def read_pitch_tier():
   lines = text_file.readlines()
   pitch = PitchTier()
   for line in lines:
+    value = 0
     if "xmin" in line:
       l = line.split()
-      pitch.xmin = l[-1]
+      pitch.xmin = float(l[-1])
     if "xmax" in line:
       l = line.split()
-      pitch.xmax = l[-1]
+      pitch.xmax = float(l[-1])
     if "number" in line:
       l = line.split()
-      pitch.points.append(l[-1])
+      number = float(l[-1])
     if "value" in line:
       l = line.split()
-      pitch.values.append(l[-1])
+      value = float(l[-1])
+      pitch.values[number] = value
   return pitch
 
 # NAME: save_modified_pitch_tier
@@ -92,13 +93,15 @@ def read_pitch_tier():
 def save_modified_pitch_tier(pitch):
   s = 'File type = "ooTextFile"\n' + \
       'Object class = "PitchTier"\n\n' + \
-      'xmin = ' + pitch.xmin + '\n' + \
-      'xmax = ' + pitch.xmax + '\n' + \
-      'points: size = ' + str(len(pitch.points)) + '\n'
-  for i in range(0,len(pitch.points)):
+      'xmin = ' + str(pitch.xmin) + ' \n' + \
+      'xmax = ' + str(pitch.xmax) + ' \n' + \
+      'points: size = ' + str(len(pitch.values)) + ' \n'
+  i = 0
+  for key, value in sorted(pitch.values.items()):
     s = s + 'points [' + str(i+1) + ']:\n' + \
-            '    number = ' + pitch.points[i] + '\n' + \
-            '    value = ' + pitch.values[i] + '\n' 
+            '    number = ' + str(key) + ' \n' + \
+            '    value = ' + str(value) + ' \n'
+    i = i + 1 
   file = open("12345.PitchTier", "w")
   file.write(s)
 
@@ -107,7 +110,43 @@ def save_modified_pitch_tier(pitch):
 # OUT: PitchTier
 # La funcion modifica el PitchTier de entrada y lo retorna.
 
-def change_pitch_tier(pitch):
+def value_mas_cercano(x, values):
+  min = 999999
+  for key in values:
+    if abs(float(key)-float(x)) < min:
+      min = abs(float(key)-float(x))
+      min_key = key
+  return float(values[min_key])
+
+def delete_rango(rango, values):
+  for key in values.keys():
+    if float(key) >= rango[0] and float(key) <= rango[1]:
+      del values[key]
+
+def change_pitch_tier(pitch, difonos):
+  di_prom = pitch.xmax/float(len(difonos))
+  rangos = []
+  rango = []
+  x = 0
+  for difono in difonos:      
+    if "A" in difono:
+      if len(rango) == 1:
+        rango.append(x + di_prom)
+        rangos.append(rango)
+        rango = []
+      elif len(rango) == 0:
+        rango.append(x)
+    x = x + di_prom
+  for rango in rangos:
+    a = 10
+    b = value_mas_cercano(rango[0],pitch.values)
+    c = 10
+    largo_intervalos = (rango[1]-rango[0])/float(c)
+    points = [rango[0] + i * largo_intervalos for i in range(0, c)]
+    delete_rango(rango, pitch.values)
+    for i in range(0,c):
+      f = a * i + b
+      pitch.values[points[i]] = f
   return pitch
 
 # NAME: add_prosodia
@@ -116,9 +155,10 @@ def change_pitch_tier(pitch):
 # La funcion toma un wav sin prosodia y se la agrega
 # reemplazando el wav original.
 
-def add_prosodia(output):
+def add_prosodia(output, difonos):
   os.system("./../praat extraer-pitch-track.praat " + output + " 12345.PitchTier 50 300")
-  pitch_tier = change_pitch_tier(read_pitch_tier())
+  pitch_tier = read_pitch_tier()
+  pitch_tier = change_pitch_tier(pitch_tier, difonos)
   save_modified_pitch_tier(pitch_tier)
   os.system("./../praat reemplazar-pitch-track.praat " + output + " 12345.PitchTier " + output + " 50 300")
 
@@ -150,7 +190,7 @@ if __name__ == '__main__':
 # Agregamos la prosodia en caso de ser necesario.
 
   if pregunta:
-    add_prosodia(output)
+    add_prosodia(output, difonos)
     os.system('rm 12345.PitchTier')
 
   os.system('rm ../difonos/script.praat')
